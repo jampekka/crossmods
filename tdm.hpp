c4d23b0a7c6f27c89cd6211be8ad282cfbb78e13
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2020-01-23 15:06:02 +0000

    Got rid of the damn boost

--- tdm.hpp
+++ tdm.hpp
@@ -1,15 +1,47 @@
 #include "common.hpp"
-#include <boost/math/distributions.hpp>
 
 namespace crossmods {
 
-using boost::math::cdf;
-using boost::math::normal;
-using boost::math::lognormal;
+using std::sqrt;
+using std::erf;
+using std::log;
 
-using ThresholdDist = lognormal;
-using LatencyDist = lognormal;
-using PassedDist = normal;
+
+
+struct NormalDistribution {
+	double mean;
+	double std;
+
+	NormalDistribution(double mean=0.0, double std=1.0)
+		: mean(mean), std(std)
+	{}
+
+	double cdf(double x) {
+		double z = (x - mean)/(sqrt(2)*std);
+		return (1.0 + erf(z))/2.0;
+	}
+};
+
+struct LognormalDistribution {
+	double mean;
+	double std;
+
+	LognormalDistribution(double mean=0.0, double std=1.0)
+		: mean(mean), std(std)
+	{}
+	
+	double cdf(double x) {
+		if(x <= 0) {
+			return 0.0;
+		}
+		double z = (log(x) - mean)/(sqrt(2)*std);
+		return (1.0 + erf(z))/2.0;
+	}
+};
+
+using ThresholdDist = LognormalDistribution;
+using LatencyDist = LognormalDistribution;
+using PassedDist = NormalDistribution;
 
 constexpr double inf = std::numeric_limits<double>::infinity();
 struct LognormalTdm {
@@ -17,7 +49,6 @@ struct LognormalTdm {
 	ThresholdDist th_d;
 	LatencyDist latency_d;
 	PassedDist passed_d;
-	double pass_threshold=0.0;
 	public:
 	
 	LognormalTdm(double thm, double ths, double lagm, double lags, double passm, double passs)
@@ -28,7 +59,9 @@ struct LognormalTdm {
 		:th_d(th_d), latency_d(latency_d), passed_d(passed_d)
 		{}*/
 	
-	CrossingPdf& decisions(CrossingPdf& out, invec taus) const {
+	CrossingPdf& decisions(CrossingPdf& out, invec taus) {
+		// TODO: We don't actually have to grid this, especially
+		// for simple trajectories
 		auto ts = out.grid;
 		auto dt = ts.dx;
 		auto N = ts.N;
@@ -37,36 +70,48 @@ struct LognormalTdm {
 		
 		double max_seen = -inf;
 		double min_seen = inf;
+		
+		auto max_decided = 0.0;
+		auto min_decided = 0.0;
+
 		double total_decided = 0.0;
 		for(size_t t=0; t < N; ++t) {
-			max_seen = std::max(max_seen, taus[t]);
-			min_seen = std::min(min_seen, taus[t]);
+			auto tau = taus[t];
+			if(!(tau > max_seen or tau < min_seen)) continue;
 			
-			// TODO: Get rid of the damn boost!
-			auto max_decided = cdf(th_d, std::max(0.0, max_seen));
-			auto min_decided = 1.0 - cdf(passed_d, min_seen);
+			if(tau > max_seen) {
+				max_seen = tau;
+				max_decided = th_d.cdf(max_seen);
+			}
+
+			if(tau < min_seen) {
+				min_seen = tau;
+				min_decided = 1.0 - passed_d.cdf(min_seen);
+			}
 			
 			auto decided = (max_decided + min_decided - max_decided*min_decided) - total_decided;
 			total_decided += decided;
 
-			
+			auto prev_p = 0.0; // = latency_d.cdf(0.0);
 			for(size_t crossing_t=t; crossing_t < N; ++crossing_t) {
 				auto lag = ts[crossing_t] - ts[t];
-				out.ps[crossing_t] += (cdf(latency_d, lag + dt) - cdf(latency_d, lag))*decided;
+				auto p = latency_d.cdf(lag + dt);
+				out.ps[crossing_t] += (p - prev_p)*decided;
+				prev_p = p;
 			}
 		}
 
-		auto total_crossed = 0.0;
+		auto uncrossed = 1.0;
 		for(size_t t=0; t < N; ++t) {
-			total_crossed += out.ps[t];
+			uncrossed -= out.ps[t];
 			out.ps[t] /= dt;
 		}
 
-		out.uncrossed = 1.0 - total_crossed;
+		out.uncrossed = uncrossed;
 		return out;
 	}
 	
-	std::unique_ptr<CrossingPdf> decisions(invec taus, double dt) const {
+	std::unique_ptr<CrossingPdf> decisions(invec taus, double dt) {
 		auto out = std::make_unique<CrossingPdf>(taus.size(), dt);
 		decisions(*out, taus);
 		return out;
