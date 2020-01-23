a87f79bc3d4fb932b46a6588c0e9527675e66061
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2020-01-23 16:58:21 +0000

    Implemented a blocked TDM

--- tdm.hpp
+++ tdm.hpp
@@ -1,4 +1,5 @@
 #include "common.hpp"
+#include <functional>
 
 namespace crossmods {
 
@@ -43,6 +44,8 @@ using ThresholdDist = LognormalDistribution;
 using LatencyDist = LognormalDistribution;
 using PassedDist = NormalDistribution;
 
+auto always_one = [](size_t t) { return 1.0; };
+
 constexpr double inf = std::numeric_limits<double>::infinity();
 struct LognormalTdm {
 	private:
@@ -55,29 +58,69 @@ struct LognormalTdm {
 		:th_d(thm, ths), latency_d(lagm, lags), passed_d(passm, passs)
 	{}
 
-	/*GenericTdm(ThresholdDist th_d, LatencyDist latency_d, PassedDist passed_d)
-		:th_d(th_d), latency_d(latency_d), passed_d(passed_d)
-		{}*/
-	
 	CrossingPdf& decisions(CrossingPdf& out, invec taus) {
+		decisions_cdf(out, taus, [](size_t t) { return 1.0; });
+		double prev = 0.0;
+		for(size_t t=0; t < out.grid.N; ++t) {
+			double p = out.ps[t];
+			out.ps[t] = (p - prev)/out.grid.dx;
+			prev = p;
+		}
+
+		return out;
+	}
+	
+	// TODO: Refactor this out
+	CrossingPdf& blocker_decisions(CrossingPdf& out, invec taus, invec taus_b) {
+		auto noearlycross = (*this);
+		noearlycross.th_d.mean = inf;
+		CrossingPdf unblocked(out.grid.N, out.grid.dx);
+		noearlycross.decisions_cdf(unblocked, taus_b, [](size_t t) { return 1.0; });
+
+		auto nolatecross = (*this);
+		nolatecross.passed_d.mean = -inf;
+		nolatecross.decisions_cdf(out, taus, [&](size_t t) { return unblocked.ps[t]; });
+
+		double prev = 0.0;
+		for(size_t t=0; t < out.grid.N; ++t) {
+			double p = out.ps[t];
+			out.ps[t] = (p - prev)/out.grid.dx;
+			prev = p;
+		}
+
+		return out;
+	}
+	
+	std::unique_ptr<CrossingPdf> blocker_decisions(invec taus, invec taus_b, double dt) {
+		auto out = std::make_unique<CrossingPdf>(taus.size(), dt);
+		blocker_decisions(*out, taus, taus_b);
+		return out;
+	}
+	
+	std::unique_ptr<CrossingPdf> decisions(invec taus, double dt) {
+		auto out = std::make_unique<CrossingPdf>(taus.size(), dt);
+		decisions(*out, taus);
+		return out;
+	}
+
+	private:
+	
+	template <typename goddamn>
+	void decisions_cdf(CrossingPdf& out, invec taus, goddamn unblocked_cdf) {
 		// TODO: We don't actually have to grid this, especially
 		// for simple trajectories
 		auto ts = out.grid;
 		auto dt = ts.dx;
 		auto N = ts.N;
-		
-		auto threshold_passed = vector<double>(N, 0.0);
-		
+
 		double max_seen = -inf;
 		double min_seen = inf;
 		
 		auto max_decided = 0.0;
 		auto min_decided = 0.0;
 
-		double total_decided = 0.0;
 		for(size_t t=0; t < N; ++t) {
 			auto tau = taus[t];
-			if(!(tau > max_seen or tau < min_seen)) continue;
 			
 			if(tau > max_seen) {
 				max_seen = tau;
@@ -89,33 +132,22 @@ struct LognormalTdm {
 				min_decided = 1.0 - passed_d.cdf(min_seen);
 			}
 			
-			auto decided = (max_decided + min_decided - max_decided*min_decided) - total_decided;
-			total_decided += decided;
-
+			auto unblocked = unblocked_cdf(t);
+			auto total_decided = (max_decided + min_decided - max_decided*min_decided);
+			total_decided *= unblocked;
+			
 			auto prev_p = 0.0; // = latency_d.cdf(0.0);
 			for(size_t crossing_t=t; crossing_t < N; ++crossing_t) {
 				auto lag = ts[crossing_t] - ts[t];
 				auto p = latency_d.cdf(lag + dt);
-				out.ps[crossing_t] += (p - prev_p)*decided;
+				out.ps[crossing_t] += total_decided*(p - prev_p);
 				prev_p = p;
 			}
 		}
 
-		auto uncrossed = 1.0;
-		for(size_t t=0; t < N; ++t) {
-			uncrossed -= out.ps[t];
-			out.ps[t] /= dt;
-		}
+		out.uncrossed = 1.0 - out.ps[N-1];
+	}
 
-		out.uncrossed = uncrossed;
-		return out;
-	}
-	
-	std::unique_ptr<CrossingPdf> decisions(invec taus, double dt) {
-		auto out = std::make_unique<CrossingPdf>(taus.size(), dt);
-		decisions(*out, taus);
-		return out;
-	}
 };
 
 }
